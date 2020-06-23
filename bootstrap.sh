bd708b8079c65bd9d85ed96a2a87b8fc695676d8
Liam Dawson <liam@ldaws.com>
2020-06-24 00:30:30 +1000

    Working homer via nginx ingress, with LE support

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 # I don't want these resources managed as part of the main kustomize stack,
 # not least because destroying them will have severe consequences
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
+kubectl apply -f vendor/cert-manager.yaml
