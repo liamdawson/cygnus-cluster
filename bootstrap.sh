17f497c8c35721dc1e0d5fb487dd687c416a67b8
Liam Dawson <liam@ldaws.com>
2020-08-14 17:39:37 +1000

    Deploy minimal podinfo

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# I don't want these resources managed as part of the main kustomize stack,
-# not least because destroying them will have severe consequences
-
-kubectl apply -f vendor/cert-manager.yaml
---
+++ infrastructure/README.md
@@ -0,0 +1,9 @@
+# Cluster Infrastructure
+
+The goal is to provide the following components, used by **TODO**
+
+- `private` ingress
+
+## Environments
+
+- `prod/cygnus`
